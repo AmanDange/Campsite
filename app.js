40dc2d12159f7aa8d58814a5fec7d290d4c9f96d
AfrinDange <dangeaman2003@gmail.com>
2023-09-02 17:51:37 +0530

    Update

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
 
-mongoose.connect('mongodb://127.0.0.1:27017/camp-site',{
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+mongoose.connect('mongodb://127.0.0.1:27017/camp-site', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -20,51 +39,110 @@ db.once("open", () => {
 const app = express();
 
 app.engine('ejs', ejsMate)
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_' 
+}))
 
-app.get('/', (req,res) => {
+const sessionConfig = {
+    name: 'session',
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+       // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dp5gvulza/", 
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    if(!['/login','/'].includes(req.originalUrl)){
+        req.session.returnTo = req.originalUrl
+    }
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-});
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not Found', 404))
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.get('/campgrounds/:id', async (req, res,) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground });
-});
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
-})
-
-app.put('/campgrounds/:id', async(req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
-});
-
-app.delete('/campgrounds/:id', async (req, res) =>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -18,11 +18,34 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random200 = Math.floor(Math.random() * 200);
+    for (let i = 0; i < 400; i++) {
+        const random500 = Math.floor(Math.random() * 500);
+        const price = Math.floor(Math.random() * 4000) + 2000;
         const camp = new Campground({
-            location: `${cities[random200].city}, ${cities[random200].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            //MY USER ID
+            author: '64db8ec8b619bd083a29868b',
+            location: `${cities[random500].city}, ${cities[random500].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque illum pariatur nostrum voluptates, provident iure dolorum eos incidunt minima praesentium, explicabo cumque dolorem sit ut facilis tempora facere beatae necessitatibus.',
+            price,
+            geometry: {
+                 type: 'Point', 
+                 coordinates: [
+                    cities[random500].longitude,
+                    cities[random500].latitude,
+                ] 
+            },
+            images:[
+                {
+                  url: 'https://res.cloudinary.com/dp5gvulza/image/upload/v1693146500/CampSite/hjbxab4bng5ocqmkrswd.jpg',  
+                  filename: 'CampSite/wsuzxhqcd4objamgmb0m',                
+                },
+                {
+                  url: 'https://res.cloudinary.com/dp5gvulza/image/upload/v1692959584/CampSite/eavulw63v9jgxty0kbs6.jpg',  
+                  filename: 'CampSite/wze73yrpn8ruwadrciwz',
+                }
+              ]      
+
         })
         await camp.save();
     }
