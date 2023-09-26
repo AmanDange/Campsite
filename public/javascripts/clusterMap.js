c1da9cd44badd7b98663e366bb3b7b8f1241dfd6
AfrinDange <dangeaman2003@gmail.com>
2023-09-26 18:12:28 +0530

    Updated upto date

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
       container: 'cluster-map',
-      style: 'mapbox://styles/mapbox/light-v10',
+      style: 'mapbox://styles/mapbox/dark-v10',
       center: [78.9629, 20.5937],
       zoom: 3.8
 });
